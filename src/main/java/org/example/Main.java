3f8c52e5fef83b4a62993088a89de2449c75320d
Ian Henderson <ian.henderson1215@gmail.com>
2023-03-21 21:02:14 -0700

    starting to fill out the UI

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,65 @@
 package org.example;
 
-public class Main implements Runnable{
+import java.util.Scanner;
+
+public class Main implements Runnable {
+    private Scanner keyboard = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
+        Main main = new Main();
+        main.run();
 
     }
 
     @Override
     public void run() {
+        printMainMenu();
+        displayModels();
+    }
+
+    private void printMainMenu() {
+
+        System.out.println("Welcome to the Tesla Configurator");
+        System.out.println();
+        System.out.println("1: Build your Tesla!");
+        System.out.println("2: Exit");
+        System.out.println();
+    }
+
+    private void displayModels(){
+        System.out.println("Model 3    ------    $");
+        System.out.println("Model Y    ------    $");
+        System.out.println("Model S    ------    $");
+        System.out.println("Model X    ------    $");
+    }
+
+    private void displayTrimLevels(){
+        System.out.println("Model 3    ------    $");
+    }
+
+    private void displayExteriorColors(){
+        System.out.println("Model 3    ------    $");
+    }
+
+    private void displayWheelTypes(){
+        System.out.println("Model 3    ------    $");
+    }
+
+    private void displayInteriorColors(){
+        System.out.println("Model 3    ------    $");
+    }
+
+    private void displayAutopilotTypes(){
+        System.out.println("Model 3    ------    $");
+    }
+
+    private void displayChargingTypes(){
+        System.out.println("Model 3    ------    $");
+    }
+
+    private void displayTotalPricing(){
 
     }
+
 }
