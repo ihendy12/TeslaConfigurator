abc0eb9e1d477bbc0cdb1d5d3cbb4ca5b0816480
Ian Henderson <ian.henderson1215@gmail.com>
2023-04-01 07:52:41 -0700

    JDBC classes added to talk to database

---
+++ src/main/java/org/example/jdbcClasses.java
@@ -0,0 +1,8 @@
+package org.example;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+public class jdbcClasses {
+    private final jdbcTemplate
+}
